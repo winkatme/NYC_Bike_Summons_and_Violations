20c0b8472f332b88fc1329d783d75e0474a52e3e
Dov Winkleman <winkatme@gmail.com>
2023-08-30 22:43:46 -0400

    commit

--- 4_NYPD_Bike_viol_Shiny/app.R
+++ 4_NYPD_Bike_viol_Shiny/app.R
@@ -9,31 +9,44 @@
 
 library(shiny)
 library(tidyverse, warn.conflicts = FALSE)
-
+library(mapview, warn.conflicts = FALSE)
+#install.packages('leaflet')
+library(leaflet)
 
 df_bike_violations<-read.csv('https://raw.githubusercontent.com/winkatme/NYC_Bike_Summons_and_Violations/main/Data/Processed/df_bike_violations_2023-08-05.csv')
+
 df_bike_violations<-df_bike_violations |> 
   mutate(description = as.factor(description)) |> 
   mutate(violation_code = as.factor(violation_code)) |>
   mutate(veh_category = as.factor(veh_category)) |>
   mutate(city_nm = as.factor(city_nm)) |>
   mutate(rpt_owning_cmd = as.factor(rpt_owning_cmd)) |>
-  mutate(violation_date = as.POSIXct(violation_date))
-
+  mutate(violation_date = as.POSIXct(violation_date)) |> 
+  drop_na()
 
 
 ui <- fluidPage(
-  selectizeInput(inputId="description_list", label="Violation Description", choices = levels(df_bike_violations$description)),
-  tableOutput("head")
-  #plotOutput("plot")
+  selectizeInput(inputId="description_input", label="Violation Description", choices = c("Choose one" = "", levels(fct_infreq(df_bike_violations$description))), selected="OPER BICYCLE WITH MORE 1 EARPHONE",  width='600px'),
+  #tableOutput("head")
+  plotOutput("map")
 
 )
 server <- function(input, output, session) {
 
-  df_head <- reactive(df_bike_violations |> filter(.data$description==.env$input$description_list))
-  output$head <- renderTable(
-    head(df_head())
-  )
+  # test - successful - just show head from input  
+  #df_head <- reactive(df_bike_violations |> filter(.data$description==.env$input$description_input))
+  # note: Since reactive turns it into a function, '()' are required after.
+  #output$head <- renderTable(head(df_head()))
+  
+  # test - success, but just in html: show mapview of just IMPROPER TURN
+  #m <- mapview(na.omit(df_bike_violations |> filter(description=="IMPROPER TURN")), xcol = "longitude", ycol = "latitude", crs = 4269, grid = FALSE)
+  #output$map <- renderLeaflet(m@map)
+  
+  # works in html
+  df_plot <- reactive(df_bike_violations |> filter(.data$description==.env$input$description_input)) 
+  #m <- reactive(mapview(df_plot(), xcol = "longitude", ycol = "latitude", crs = 4269, grid = FALSE))
+  output$map <- renderLeaflet((mapview(df_plot(), xcol = "longitude", ycol = "latitude", crs = 4269, grid = FALSE)@map))
+  
   
 }
 
